b31c30f3b0263a05a28d92d553d5628790b960d0
Emil-das-rosa-Einhorn <niklaskobs@gmail.com>
2026-09-07 14:04:47 +0200

    Add update check and user prompt in main loop; enhance menu interaction for game updates

--- RPG-Main.py
+++ RPG-Main.py
@@ -285,6 +285,10 @@ def menu ():
                         break
                     elif choice == "B":
                         break
+                    else:
+                        os.system("cls" if os.name == "nt" else "clear")
+                        print ("Please choose a valid option")
+                        sleep (ui_wait_time)
                 os.system("cls" if os.name == "nt" else "clear")
             else:
                 os.system("cls" if os.name == "nt" else "clear")
@@ -420,6 +424,33 @@ def main():
     current_scene = 1
     try:
         while True:
+            check, outofdate = check_updater()
+            if check:
+                os.system("cls" if os.name == "nt" else "clear")
+                while True:
+                    print ("There is a new Update")
+                    print (f"This files will be Updated: {outofdate}")
+                    print ("-"*50)
+                    print ("A: Update Game")
+                    print ("B: Exit")
+                    print ("-"*50)
+                    choice = input ("Please chose an Option: ").upper()
+                    if choice == "A":
+                        lounch_updater(outofdate)
+                        break
+                    elif choice == "B":
+                        break
+                    else:
+                        os.system("cls" if os.name == "nt" else "clear")
+                        print ("Please choose a valid option")
+                        sleep (ui_wait_time)
+                os.system("cls" if os.name == "nt" else "clear")
+            else:
+                os.system("cls" if os.name == "nt" else "clear")
+                print ("No updates of you to day")
+                sleep(ui_wait_time)
+                os.system("cls" if os.name == "nt" else "clear")
+
             while True:
                 print ("Main Menu:")
                 menu()
