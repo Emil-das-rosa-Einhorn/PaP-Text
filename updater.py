b324e06d68b87bc62c3ed5214f229de8beb370c8
Emil-das-rosa-Einhorn <niklaskobs@gmail.com>
2026-09-07 11:18:34 +0200

    Update updater.py to improve update info download functionality and clean up commented code

--- updater.py
+++ updater.py
@@ -5,19 +5,28 @@ import requests
 import json
 import os
 
-version = sys.argv[1]
-url = sys.argv[2]
+#l_version = sys.argv[1]
+#m_version = sys.argv[2]
 
-print("Neue Version:", version)
-print("Download:", url)
+#print("Neue Version:", l_version)
+#print("Download:", m_version)
 
-def download_gamefile():
-    filename = "update-info"
+def download_update_info():
+    filename = "update-infos"
     try:
-        base_url = "https://raw.githubusercontent.com/Emil-das-rosa-Einhorn/PaP-Text/refs/heads/main/"
+        base_url = "https://raw.githubusercontent.com/Emil-das-rosa-Einhorn/PaP-Text/refs/heads/main/updates/"
         url = base_url + filename + ".json"
+        print (url)
         pfad = os.path.join(os.path.dirname(__file__), "updates", "info.json")
+        print (pfad)
         urllib.request.urlretrieve(url, pfad)
         return True, "Download erfolgreich"
     except Exception as e:
         return False, e
+
+def main ():
+    download_update_info()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
